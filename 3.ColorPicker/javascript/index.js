6306a2e58da952180e6041e4dc4e1e664c03a74f
Yamini <yaminiyam0326@gmail.com>
2024-05-17 13:54:36 -0500

    completedcode

---
+++ 3.ColorPicker/javascript/index.js
@@ -0,0 +1,18 @@
+let backgroundcolor=document.getElementById("pageContainer");
+let buttonclick=document.getElementById("buttons");
+function button1(){
+    backgroundcolor.style.backgroundColor='#e0e0e0';
+    buttonclick.textContent='#e0e0e0';
+}
+function button2(){
+    backgroundcolor.style.backgroundColor='#6fcf97';
+    buttonclick.textContent='#6fcf97';
+}
+function button3(){
+    backgroundcolor.style.backgroundColor='#56ccf2';
+    buttonclick.textContent='#56ccf2';
+}
+function button4(){
+    backgroundcolor.style.backgroundColor='#bb6bd9';
+    buttonclick.textContent='#bb6bd9';
+}
